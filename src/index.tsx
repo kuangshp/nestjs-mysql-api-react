5bfb194e7c58f4243fa823d15665344707179f31
kuangshp <kuangshp@126.com>
2021-03-30 16:36:10 +0800

    feat: 数据持久化

--- src/index.tsx
+++ src/index.tsx
@@ -11,10 +11,27 @@ import zhCN from 'antd/es/locale/zh_CN';
 import routeConfig from 'src/router';
 import 'moment/locale/zh-cn';
 
+const localStorageState: string = '@@LOCAL_STORAGE_STATE';
+const getStorageData = () => {
+  const state = window.localStorage.getItem(localStorageState);
+  if (state) {
+    return typeof state === 'string' ? JSON.parse(state) : state;
+  } else {
+    return {};
+  }
+};
+
 const app = dva({
   history: createHistory({
     basename: 'admin-web', // 配置项目名,可以不写
   }),
+  // 数据持久化
+  initialState: getStorageData(),
+  onStateChange: function () {
+    const preState = getStorageData();
+    // eslint-disable-next-line
+    localStorage.setItem(localStorageState, JSON.stringify(Object.assign(preState, arguments[0])));
+  },
 });
 
 app.use(createLoading());
